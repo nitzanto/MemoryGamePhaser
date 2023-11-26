8ac74b561111c81d86d97a78dda133ce5cf388b0
nitzanto <nitzan16001@gmail.com>
2023-11-26 22:49:32 +0200

    * Added emojis

--- src/Alerts/GameState/endGameState.ts
+++ src/Alerts/GameState/endGameState.ts
@@ -3,8 +3,8 @@ import Swal from "sweetalert2";
 export const endGameState = (restartGame: () => void, isWin: boolean) => {
   const title = isWin ? "Congratulations!" : "Game Over";
   const text = isWin
-    ? "You won the game!"
-    : "You ran out of turns. Better luck next time.";
+    ? "You won the game! 😊"
+    : "You ran out of turns. Better luck next time. 😭";
 
   Swal.fire({
     icon: isWin ? "success" : "error",
