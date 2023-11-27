a3938add3b458f2a21ac40963ea1a6c531a44477
nitzanto <nitzan16001@gmail.com>
2023-11-27 15:00:25 +0200

    * update

--- src/libs/common/Interfaces/GameManager.ts
+++ src/libs/common/Interfaces/GameManager.ts
@@ -1,9 +1,5 @@
 export interface GameManager {
   initGame(): void;
 
-  generateGameBoard(
-    rows: number,
-    columns: number,
-    numCards: number,
-  ): () => string[];
+  generateGameBoard(rows: number, columns: number, numCards: number): string[];
 }
